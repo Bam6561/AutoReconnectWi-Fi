66ede31522f1bec7c1fd81a42d33c7251cbb6afc
Danny Nguyen <ndanny09@gmail.com>
2021-11-04 21:26:52 -0400

    Update autoReconnectWiFi.bat
    - Resets network Wi-Fi adapter if connecting to an internet connection isn't able to get a ping response

--- autoReconnectWiFi.bat
+++ autoReconnectWiFi.bat
@@ -2,6 +2,14 @@
 	ping google.com -n 1 
 	if errorlevel 1 (
 		netsh wlan connect "WIFI_NAME"
+		timeout 5
+		ping google.com -n 1
+		if errorlevel 1 (
+			netsh interface set interface Wi-Fi disable
+			netsh interface set interface Wi-Fi enable
+			timeout 5
+			netsh wlan connect "WIFI_NAME"
+		)
 	) 
 	timeout 1800
 	goto :autoReconnectWiFi
